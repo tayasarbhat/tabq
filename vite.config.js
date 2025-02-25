a7d1ef9d8b756c87411f45e01346396cd3931039
tayasarbhat <tayasarbhat@gmail.com>
2025-02-25 23:36:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tabq/', // Replace 'your-repo-name' with the actual repository name
-  plugins: [react()],
+  server: {
+    port: 3000
+  }
 });
